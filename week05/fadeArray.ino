04e2f2b3953779ce53c52083b0b7e28d32e33943
Andy Doro <mail@andydoro.com>
2018-10-10 11:22:05 -0400

    new examples
    examples

--- week05/fadeArray.ino
+++ week05/fadeArray.ino
@@ -1,4 +1,3 @@
-
 int ledPin[6] = {3, 5, 6, 9, 10, 11};
 int fadeValue[6] = {0, 51, 102, 153, 204, 255};
 int fadeIncrement[6] = {1, 1, 1, 1, 1, -1};
@@ -6,11 +5,26 @@ int fadeIncrement[6] = {1, 1, 1, 1, 1, -1};
 void setup() {
   // put your setup code here, to run once:
 
+  Serial.begin(9600);
+
   // set all to OUTPUT
   for (int i = 0; i < 6; i++) {
     pinMode(ledPin[i], OUTPUT);
   }
 
+  // blink all!
+  for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 6; i++) {
+      digitalWrite(ledPin[i], HIGH);
+    }
+    delay(200);
+    for (int i = 0; i < 6; i++) {
+      digitalWrite(ledPin[i], LOW);
+    }
+    delay(200);
+  }
+
+  delay(500);
 
 }
 
@@ -26,5 +40,10 @@ void loop() {
     } else if (fadeValue[i] > 254) { // if value goes too high...
       fadeIncrement[i] = -1;
     }
+    Serial.print(fadeValue[i]);
+    Serial.print("\t");
   }
+  Serial.println("");
+  //delay(1);
+
 }
